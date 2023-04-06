9ddbbe65fa20aa26be6307cbe62b2151b0e6b203
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-04-05 20:34:37 -0400

    update home page bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <h1 id="name">Jor&shy;dan<br />Step&shy;hen&shy;sen</h1>
     <br />
     <h3>
-      Hello! I'm a designer from Winnipeg. I'm mainly focused on interface design, design systems, branding, and type design.<br /><br />
+      Hello! I'm Jordan — a multi-disciplinary designer based in Toronto. I'm mainly focused on interface design, design systems, branding, and type design.<br /><br />
       Currently doing product design at <a href="https://www.boldcommerce.com" target="_blank" rel="noreferrer">Bold Commerce</a><br /><br />
       Taking on work on a limited basis. <a href="mailto:jordanstephensen@gmail.com" target="_blank" rel="noreferrer">Contact me</a> for work samples or to discuss rates.
     </h3><br />
