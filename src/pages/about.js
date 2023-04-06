d6a71ce476bbe1e4d99137ef22b57804b87b2112
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-04-06 14:33:41 -0400

    adjustments

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,11 +9,11 @@ import me from '../images/JordanStephensen.png'
 const About = () => (
   <Layout>
     <SEO title="About" />
-    <div className="contents__text-container">
-      <h3>Hello, I'm Jordan.<br /></h3> 
-    </div>
+    {/* <div className="contents__text-container">
+    </div> */}
     <div className="contents__text-container">
       <div className="contents__text-container--2col">
+        <h3>Hello, I'm Jordan.<br /></h3> 
         <p>
           I'm a designer in Toronto. Originally from Winnipeg, In 2013 I graduated from the University of Manitoba with a Bachelor of Fine Arts Honours. Areas of study while there included graphic design, illustration, and art history.<br /><br />
           From 2015–2019 I worked at <Link href="https://www.relishbranding.ca" target="_blank">Relish Branding</Link> as a graphic designer, mainly focusing on branding, advertising, and UX/UI.<br /><br />
--- src/pages/☮.js
+++ src/pages/☮.js
@@ -5,13 +5,12 @@ import SEO from "../components/seo"
 
 const InnerPeace = () => (
     <peacecontainer><SEO title="Inner Peace" />
-    <Link id="peacelink" to="/">‹</Link>
-    <div id="outerglow">
-    <div id="innerglow" />
-            <iframe class="secrets" width="560" height="315" src="https://www.youtube.com/embed/3De7YzwkbpI?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <p class="secrets">A little break room, just enjoy the gradients</p>
-            <h3>Take a minute to breathe</h3>
-    </div>
+        <Link id="peacelink" to="/">‹</Link>
+        <div id="outerglow">
+        <div id="innerglow" />
+            <h3 className="inner-peace__text">Take a minute</h3>
+        </div>
+        <iframe className="secrets" width="560" height="315" src="https://www.youtube.com/embed/3De7YzwkbpI?autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </peacecontainer>
     )
 
