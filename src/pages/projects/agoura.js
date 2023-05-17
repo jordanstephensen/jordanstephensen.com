eb847ae7da81e9154512031e2d6979620b97bda8
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-05-17 12:47:17 -0400

    add agoura characters

--- src/pages/projects/agoura.js
+++ src/pages/projects/agoura.js
@@ -3,9 +3,23 @@ import React from "react"
 import Layout from "../../components/layout/layout"
 import SEO from "../../components/seo"
 
+import characters from "../../images/agoura/agoura-characters.svg"
+
 const Agoura = () => (
-  <Layout title="Coming Soon">
-    <SEO title="Agoura Display Font" />
+  <Layout title="Agoura Display">
+    <SEO title="Eternal Monospace Display Font" />
+    <div className="contents__text-container">
+      <div className="contents__text-container--2col">
+        <h3>Not yet released<br/>
+        </h3>
+      </div>
+      <div className="contents__text-container--2col">
+        <p>
+        A display serif inspired by the high-contrast type of the 60’s and 70’s. Work in progress.
+        </p>
+      </div>
+    </div>
+    <img className="image--body" src={characters} alt="Agoura display font"/>
   </Layout>
 )
 
