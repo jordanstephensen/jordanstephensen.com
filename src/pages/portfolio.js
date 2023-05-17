a10a301a95330b9dc357d48ee8168e4127499e3b
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-05-17 13:18:52 -0400

    portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,11 +6,13 @@ import SEO from "../components/seo"
 // import portfolio from "../../images/JordanStephensen-Portfolio.pdf"
 
 const Portfolio = () => (
-  <Layout title="Branding Portfolio">
-    <SEO title="Branding Portfolio" />
-    <a href={`/static/JordanStephensen-Portfolio.pdf`} target="_blank" rel="noreferrer" id="email">
-        <h3>Portfolio</h3>
-    </a>
+  <Layout title="Portfolio">
+    <SEO title="Portfolio" />
+    <h3>
+        <a href="https://drive.google.com/file/d/19FgzjGnbao9daAXgOHPYH7_qOKHMWEu-/view?usp=share_link" target="_blank">
+            Branding and Typography Portfolio
+        </a>
+    </h3>
   </Layout>
 )
 
