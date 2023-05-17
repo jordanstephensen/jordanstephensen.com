e46214aecb3b4601b700639005c489127bdbc1e6
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-05-17 11:48:46 -0400

    copy adjustment

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,7 +18,7 @@ const About = () => (
           I'm a designer in Toronto. Originally from Winnipeg, In 2013 I graduated from the University of Manitoba with a Bachelor of Fine Arts Honours. Areas of study while there included graphic design, illustration, and art history.<br /><br />
           From 2015–2019 I worked at <Link href="https://www.relishbranding.ca" target="_blank">Relish Branding</Link> as a graphic designer, mainly focusing on branding, advertising, and UX/UI.<br /><br />
           From 2019 to the present, I have been working at Winnipeg-based <Link href="https://www.boldcommerce.com" target="_blank">Bold Commerce</Link> as a product designer, focusing on UI design and design systems work.<br /><br />
-          Outside of work, I dabble in type design and illustration, and am currently learning 3D design. I love running and yoga and spending time outdoors with my dog Mase
+          Outside of work, I dabble in type design and illustration, and am currently learning 3D design. I love running and yoga and spending time outdoors with my dog Mase.
         </p>  
       </div>
       <div className="contents__text-container--2col">
