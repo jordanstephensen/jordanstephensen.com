25c998a5bafdab98e8bcccdf99a329087ec4cc83
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-06-28 01:31:08 -0400

    add page

---
+++ src/pages/work/bar-eclipse.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import Layout from "../../components/layout/layout"
+import SEO from "../../components/seo"
+
+import characters from "../../images/agoura/agoura-characters.svg"
+
+const Agoura = () => (
+  <Layout title="Bar Eclipse">
+    <SEO title="Bar Eclipse" />
+    <div className="contents__container">
+      <div className="contents__container--column">
+        <h3>Coming soon<br/>
+        </h3>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default Agoura
