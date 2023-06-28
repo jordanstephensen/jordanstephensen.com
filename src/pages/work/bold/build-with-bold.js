86058686097545301ba15a27d4c5595f4054e50f
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-06-28 01:17:46 -0400

    fix casing error

--- src/pages/work/bold/build-with-bold.js
+++ src/pages/work/bold/build-with-bold.js
@@ -3,11 +3,12 @@ import React from "react"
 import Layout from "../../../components/layout/layout"
 import SEO from "../../../components/seo"
 
-// import bevycover from "../../../images/bold/bevy/Bevy-Cover.png"
+import bevycover from "../../../images/bold/bevy/bevy-cover.png"
 
 const Agoura = () => (
   <Layout title="Empowering Developers to Build with Bold">
     <SEO title="Empowering Developers to Build with Bold" />
+    <img className="contents__container" src={bevycover} alt="An overview of Bevy's component library"/>
     </Layout>
 )
 
