a2d86c926190fb2be80329a2fd0566fc65cb8ad2
jordanstephensen <47726286+jordanstephensen@users.noreply.github.com>
2023-06-30 15:40:57 -0400

    about changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,12 +11,10 @@ const About = () => (
     <div className="contents__container">
       <div className="contents__container--column">
         <h3>Hello, I'm Jordan.<br /></h3> 
-        <p>
-          I'm a designer in Toronto. Originally from Winnipeg, In 2013 I graduated from the University of Manitoba with a Bachelor of Fine Arts Honours. Areas of study while there included graphic design, illustration, and art history.<br /><br />
-          From 2015–2019 I worked at <Link href="https://www.relishbranding.ca" target="_blank">Relish Branding</Link> as a graphic designer, mainly focusing on branding, advertising, and UX/UI.<br /><br />
-          From 2019 to the present, I have been working at Winnipeg-based <Link href="https://www.boldcommerce.com" target="_blank">Bold Commerce</Link> as a product designer, focusing on UI design and design systems work.<br /><br />
-          Outside of work, I dabble in type design and illustration, and am currently learning 3D and game design in Blender and Unity respecively. I love running and yoga and spending time outdoors with my dog Mase.
-        </p>  
+        <p>I'm a designer living in Toronto. Originally from Winnipeg, In 2013 I graduated from the University of Manitoba with a Bachelor of Fine Arts Honours. Areas of study while there included graphic design, illustration, and art history.</p>
+        <p>From 2015–2019 I worked at <Link href="https://www.relishbranding.ca" target="_blank">Relish Branding</Link> as a graphic designer, mainly focusing on branding, advertising, and UX/UI.</p>
+        <p>From 2019 to the present, I have been working at Winnipeg-based <Link href="https://www.boldcommerce.com" target="_blank">Bold Commerce</Link> as a product designer, focusing on UI design and design systems work.</p>
+        <p>Outside of work, I dabble in type design and illustration, and am currently learning 3D and game design in Blender and Unity respecively. I love running and yoga and spending time outdoors with my dog Mase.</p>  
       </div>
         <img class="contents__container--column image--about-photo" src={me} alt="Myself" />
     </div>
